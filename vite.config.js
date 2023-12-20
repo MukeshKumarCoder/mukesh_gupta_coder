85f03cbce96af6f01e08039b6f71b2ec678afae8
magupta1 <mg328790@gmail.com>
2023-12-20 17:19:24 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mukesh_gupta/"
+  base: "/mukesh_gupta_coder/"
 })
